35ce2a01784a099506a17a22ccfb556d3c1927da
223880 <rafael.segala45@gmail.com>
2024-06-26 12:10:45 -0300

    Initial sign to Rust

--- frost rust/sign.rs
+++ frost rust/sign.rs
@@ -1,1 +1,14 @@
+use std::collections::HashMap;
+use std::fs::File;
 
+use crate::Sign;
+
+pub fn load_signs() -> HashMap<String, Sign> {
+
+    let mut signs = HashMap::new();
+    let file = File::open("signs.json").unwrap();
+    let signs_json: Vec<Sign> = serde_json::from_reader(file).unwrap();
+    for sign in signs_json {
+        signs.insert(sign.name.clone(), sign);
+    }
+    
